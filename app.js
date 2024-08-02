e3fbbedc8e209e68b6dc86b469415bf49111b93c
Vakula Uladimir <42750233+vakulio@users.noreply.github.com>
2024-08-02 21:48:22 +0300

    create new project

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { HTMX_KNOWLEDGE } from './data/htmx-info.js';
+const courseGoals = [];
 
 const app = express();
 
@@ -9,59 +9,43 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>HTMX Essentials</title>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-          
-        />
-        <link rel="icon" href="/icon.png" />
-        <script src="/htmx.js" defer></script>
-        <link rel="stylesheet" href="/main.css" />
-      </head>
-      <body>
-        <header id="main-header">
-          <img src="/htmx-logo.jpg" alt="HTMX Logo" />
-          <h1>Essentials</h1>
-        </header>
-
-         <main>
-          <p>HTMX is a JavaScript library that you use without writing JavaScript code.</p>
-          <form hx-post="/note" hx-target="ul" hx-swap="outerHTML">
-            <p>
-              <label for="note">Your note</label>
-              <input type="text" id="note" name="note">
-            </p>
-            <p>
-              <button>Save Note</button>
-            </p>
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Learn HTMX</title>
+      <link rel="stylesheet" href="/main.css" />
+      <script src="/htmx.js" defer></script>
+    </head>
+    <body>
+      <main>
+        <h1>Manage your course goals</h1>
+        <section>
+          <form id="goal-form">
+            <div>
+              <label htmlFor="goal">Goal</label>
+              <input type="text" id="goal" name="goal" />
+            </div>
+            <button type="submit">Add goal</button>
           </form>
-          <ul>
-            ${HTMX_KNOWLEDGE.map((info) => `<li>${info}</li>`).join('')}
+        </section>
+        <section>
+          <ul id="goals">
+          ${courseGoals.map(
+            (goal, index) => `
+            <li id="goal-${index}">
+              <span>${goal}</span>
+              <button>Remove</button>
+            </li>
+          `
+          )}
           </ul>
-        </main>
-      </body>
-    </html>
+        </section>
+      </main>
+    </body>
+  </html>
   `);
 });
-
-app.get('/learn-more', (req, res) => {
-    res.send(`
-      <ul>
-        ${HTMX_KNOWLEDGE.map(item => `<li>${item}</li>`).join('')}
-      </ul>`)
-})
-
-app.post('/note', (req, res) => {
-  const note = req.body.note;
-  HTMX_KNOWLEDGE.unshift(note);
-  res.send(`
-    <ul>
-      ${HTMX_KNOWLEDGE.map(item => `<li>${item}</li>`).join('')}
-    </ul>`)
-})
 
 app.listen(3000);
