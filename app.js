60a6ee9d3c696d4970dc7578401cc24cef248a2e
Vakula Uladimir <42750233+vakulio@users.noreply.github.com>
2024-08-04 12:38:24 +0300

    add and delete

--- app.js
+++ app.js
@@ -23,9 +23,16 @@ app.post('/places', (req, res) => {
   const location = AVAILABLE_LOCATIONS.find((loc) => loc.id === locationId);
   INTERESTING_LOCATIONS.push(location);
 
+  const availableLocations = AVAILABLE_LOCATIONS.filter(
+    (location) => !INTERESTING_LOCATIONS.includes(location)
+  );
+
   res.send(`
-    TODO
-  `);
+    ${renderLocation(location, false)}
+    
+    <ul id="available-locations" class="locations" hx-swap-oob="true">
+        ${availableLocations.map((location) => renderLocation(location)).join('')}
+    </ul>`);
 });
 
 app.delete('/places/:id', (req, res) => {
@@ -35,7 +42,14 @@ app.delete('/places/:id', (req, res) => {
   );
   INTERESTING_LOCATIONS.splice(locationIndex, 1);
 
-  res.send();
+  const availableLocations = AVAILABLE_LOCATIONS.filter(
+    (location) => !INTERESTING_LOCATIONS.includes(location)
+  );
+
+  res.send(`
+    <ul id="available-locations" class="locations" hx-swap-oob="true">
+        ${availableLocations.map((location) => renderLocation(location)).join('')}
+    </ul>`);
 });
 
 app.listen(3000);
--- views/components/location.js
+++ views/components/location.js
@@ -1,7 +1,19 @@
-export default function renderLocation(location) {
+export default function renderLocation(location, isAvaliable = true) {
+  let attributes;
+  if (isAvaliable) {
+    attributes = `hx-post="/places" 
+      hx-vals='{"locationId": "${location.id}"}'
+      hx-target="#interesting-locations"
+      hx-swap="beforeend show:top"`;
+  } else {
+    attributes = `hx-delete="/places/${location.id}"
+      hx-confirm='Are you sure you want to delete this location?'
+      hx-target="closest li"
+      hx-swap="outerHTML"`;
+  }
   return `
     <li class="location-item">
-      <button>
+      <button ${attributes}>
         <img src="${`/images/${location.image.src}`}" alt="${location.image.alt}" />
         <h3>${location.title}</h3>
       </button>
